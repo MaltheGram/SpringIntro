b222c7640e6bc5cc8f74990ca8ecef25ec96d8a3
MaltheGram <malthegram22@gmail.com>
2021-09-26 14:42:37 +0200

    seperated logic from controller

--- src/main/java/com/example/springintro/Controller/Controller.java
+++ src/main/java/com/example/springintro/Controller/Controller.java
@@ -1,11 +1,11 @@
 package com.example.springintro.Controller;
 
-import org.apache.tomcat.jni.Local;
+import com.example.springintro.Services.isItFriday;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
 import java.time.LocalDate;
 
 
@@ -36,33 +36,9 @@ public class Controller{
 
    @GetMapping("/erdetfredag")
     public String erDetFredag(){
-        String plainPrint = "It's not friday. It's " + LocalDate.now().getDayOfWeek();
-        String htmlPrint = "<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <title>IT'S FRIDAY</title>\n" +
-                "</head>\n" +
-                "\n" +
-                "<body style=\"background-color: steelblue\">\n" +
-                "\n" +
-                "<h1 style=\"color: red\\\">IT'S FRIDAY</h1>" +
-                "</body>\n" +
-                "</html>";
 
-        int q = LocalDate.now().getDayOfMonth();
-        int m = LocalDate.now().getMonthValue();
-        int j = LocalDate.now().getYear() / 100;
-        int k = LocalDate.now().getYear() % 100;
+        return "Is it friday? " + isItFriday.fridayCalculator();
 
-        int h = (q + (13*(m + 1) / 5) + k +(k / 4) + (j / 4) -2 * j) % 7;
-
-        int d = ((h + 5)%7) +1;
-
-
-        String print = d == 5 ? htmlPrint : plainPrint;
-
-        return print;
    }
 
 
---
+++ src/main/java/com/example/springintro/Services/isItFriday.java
@@ -0,0 +1,44 @@
+package com.example.springintro.Services;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class isItFriday {
+
+    public isItFriday() {
+    }
+
+    public static String fridayCalculator() {
+        DayOfWeek currentDay = LocalDate.now().getDayOfWeek();
+        String plainPrint = "It's not friday. It's " + currentDay;
+
+        String htmlPrint = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>IT'S FRIDAY</title>\n" +
+                "</head>\n" +
+                "\n" +
+                "<body style=\"background-color: steelblue\">\n" +
+                "\n" +
+                "<h1 style=\"color: red\\\">IT'S FRIDAY</h1>" +
+                "</body>\n" +
+                "</html>";
+
+
+        int q = LocalDate.now().getDayOfMonth();
+        int m = LocalDate.now().getMonthValue();
+        int j = LocalDate.now().getYear() / 100;
+        int k = LocalDate.now().getYear() % 100;
+
+        int h = (q + (13 * (m + 1) / 5) + k + (k / 4) + (j / 4) - 2 * j) % 7;
+
+        int d = ((h + 5) % 7) + 1;
+
+        // Ternary operator: Expression ? trueCase : elseCase
+        String print = d == 5 ? htmlPrint : plainPrint;
+
+        return print;
+
+    }
+}
